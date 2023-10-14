6e7a90ac3f83a1e13d55b1d6aff0f8b87b2fc66c
ckopec <c_kopec@hotmail.com>
2023-10-14 15:37:08 -0400

    Added SqlHelper

---
+++ Core/ObjectParameter.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Core
+{
+    public class ObjectParameter
+    {
+        public string Name;
+        public object Value;
+
+        public ObjectParameter(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
---
+++ Core/SqlHelper.cs
@@ -0,0 +1,140 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Core
+{
+    public static class SqlHelper
+    {
+        public const int TIMEOUT = 30; // in seconds
+
+        public static DataTable Read(string connectionString, string sqlStatement)
+        {
+            return Read(connectionString, sqlStatement, new List<SqlParameter>());
+        }
+
+        public static DataTable Read(string connectionString, string sqlStatement, List<ObjectParameter> parameters)
+        {
+            return Read(connectionString, sqlStatement, parameters.ToSqlParameterList());
+        }
+
+        public static DataTable Read(string connectionString, string sqlStatement, List<SqlParameter> parameters)
+        {
+            DataSet ds = new();
+
+            SqlConnection sqlConnection = new(connectionString);
+            using (SqlConnection cnn = sqlConnection)
+            {
+                SqlCommand cmd = GetSqlCommand(cnn, sqlStatement, parameters);
+                SqlDataAdapter da = new(cmd);
+                cnn.Open();
+                da.Fill(ds);
+                ds.Dispose();
+                cnn.Close();
+            }
+
+            return ds.Tables[0];
+        }
+
+        public static int Execute(string connectionString, string sqlStatement)
+        {
+            return Execute(connectionString, sqlStatement, new List<SqlParameter>());
+        }
+
+        public static int Execute(string connectionString, string sqlStatement, List<ObjectParameter> parameters)
+        {
+            return Execute(connectionString, sqlStatement, parameters.ToSqlParameterList());
+        }
+
+        public static int Execute(string connectionString, string sqlStatement, List<SqlParameter> parameters)
+        {
+            using SqlConnection cnn = new(connectionString);
+            cnn.Open();
+
+            SqlCommand cmd = GetSqlCommand(cnn, sqlStatement, parameters);
+            int rowsAffected = cmd.ExecuteNonQuery();
+
+            cnn.Close();
+
+            return rowsAffected;
+        }
+
+        private static SqlCommand GetSqlCommand(SqlConnection cnn, string sql, List<SqlParameter> parameters)
+        {
+            SqlCommand cmd = new(sql, cnn)
+            {
+                CommandTimeout = TIMEOUT
+            };
+
+            foreach (SqlParameter param in parameters)
+            {
+                _ = new SqlParameter(param.ParameterName, param.Value);
+
+                if (param.Value == null)
+                {
+                    cmd.Parameters.Add(new SqlParameter(param.ParameterName, DBNull.Value));
+                }
+                else
+                {
+                    cmd.Parameters.Add(param);
+                }
+            }
+
+            return cmd;
+        }
+
+        public static SqlParameter[]? ToArray(this List<SqlParameter> parameters)
+        {
+            if (parameters == null)
+                return null;
+
+            if (parameters.Count == 0)
+                return null;
+
+            SqlParameter[] sp = new SqlParameter[parameters.Count];
+
+            for (int i = 0; i < parameters.Count; i++)
+            {
+                sp[i] = parameters[i];
+            }
+
+            return sp;
+        }
+
+        public static List<SqlParameter> ToSqlParameterList(this List<ObjectParameter> parameters)
+        {
+            List<SqlParameter> p = new();
+
+            foreach (ObjectParameter o in parameters)
+                p.Add(new SqlParameter(o.Name, o.Value));
+
+            return p;
+        }
+
+        public static List<string> ToSearchTerms(this string searchString)
+        {
+            List<string> lst = new();
+
+            if (!String.IsNullOrWhiteSpace(searchString))
+            {
+                searchString = searchString.Trim();
+
+                string[] ssArray = searchString.Split(' ');
+
+                foreach (string s in ssArray)
+                {
+                    if (!String.IsNullOrWhiteSpace(s.Trim()))
+                        lst.Add(s.Trim());
+                }
+            }
+
+            return lst;
+        }
+
+        public static DataTable GetPrimaryKeys(string connectionString, string tableName)
+        {
+            string sql = String.Format("sp_pkeys '{0}'", tableName);
+
+            return Read(connectionString, sql);
+        }
+    }
+}
