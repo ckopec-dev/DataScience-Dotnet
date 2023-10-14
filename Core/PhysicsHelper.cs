6641ee3b9639fdfb5860fdfcb7faf12db3499331
ckopec <c_kopec@hotmail.com>
2023-10-14 14:37:53 -0400

    Added PhysicsHelper

---
+++ Core/PhysicsHelper.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Core
+{
+    public static class PhysicsHelper
+    {
+        public static double Decay(double initialQuantity, double halfLife, double elapsedTime)
+        {
+            return initialQuantity * Math.Pow(0.5d, (elapsedTime / halfLife));
+        }
+    }
+}
