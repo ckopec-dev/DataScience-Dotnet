c67f2a34eafe2fa16caa66a7d9c2ff3d52af9562
ckopec <c_kopec@hotmail.com>
2023-10-29 14:07:28 -0400

    Added new helpers

---
+++ Core/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Core
+{
+    public static class FileHelper
+    {
+        public static string GetProjectPath()
+        {
+            string path = Environment.CurrentDirectory;
+
+            DirectoryInfo? di = Directory.GetParent(path);
+
+            if (di != null)
+                di = di.Parent;
+            if (di != null)
+                di = di.Parent;
+            if (di != null)
+                return di.FullName;
+            else
+                throw new Exception("Unable to determine project path.");
+        }
+    }
+}
