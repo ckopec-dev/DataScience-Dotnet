e38e2c7cc5a5d1cef56af0efa4cba74ffe918b14
ckopec <c_kopec@hotmail.com>
2023-11-19 15:37:09 -0500

    Added HeapSort

--- Core/SortHelper.cs
+++ Core/SortHelper.cs
@@ -57,9 +57,49 @@
             }
         }
 
+        public static void HeapSort(int[] data)
+        {
+            HeapSort(data, data.Length);
+        }
+
+        public static void HeapSort(int[] arr, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(arr, n, i);
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+                Heapify(arr, i, 0);
+            }
+        }
+
         #endregion
 
         #region Helper methods
+
+        private static void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest])
+                largest = left;
+
+            if (right < n && arr[right] > arr[largest])
+                largest = right;
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+                Heapify(arr, n, largest);
+            }
+        }
 
         #endregion
     }
