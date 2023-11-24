e1a0052d6431682e13baea85f954803f2b417866
ckopec <c_kopec@hotmail.com>
2023-11-24 17:31:11 -0500

    Removed Riemann project

--- Riemann/Program.cs
+++ Riemann/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Riemann
+﻿using System.Numerics;
+
+namespace Riemann
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Zeta(new Complex(-0.5d, 0));
+        }
+
+        public static Complex Zeta(Complex s)
+        {
+            // 1 + 1/2^s + 1/3^s ...
+
+            const double terms = 10d;
+
+            Complex c = new Complex(-1d, 8d);
+
+            for(double i = 2; i < terms; i++)
+            {
+                c += (1 / Complex.Pow(new Complex(i, 0), s));
+
+                Console.WriteLine("c: {0}", c);
+            }
+
+            return c;
         }
     }
 }
