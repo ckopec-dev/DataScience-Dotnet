6b2beca196cb3960a1deffef2adbe3859b3b03a0
ckopec <c_kopec@hotmail.com>
2023-11-27 19:37:34 -0500

    Added GetHtmlAsync

--- Core/NetworkHelper.cs
+++ Core/NetworkHelper.cs
@@ -3,5 +3,14 @@ namespace Core
 {
     public static class NetworkHelper
     {
+        public static async Task<string> GetHtmlAsync(string url)
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Core");
+
+            var content = await client.GetStringAsync(url);
+
+            return content;
+        }
     }
 }
