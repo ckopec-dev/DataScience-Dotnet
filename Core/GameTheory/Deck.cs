be3d28bdaa970485d911835961ba8f6be9026e9c
ckopec-dev <c_kopec@hotmail.com>
2024-09-02 11:34:07 -0400

    Added Deck class

---
+++ Core/GameTheory/Deck.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace Core.GameTheory
+{
+    public class Deck
+    {
+        public Queue<Card> Cards { get; set; }
+
+        public Deck()
+        {
+            // A deck consists of one card of every suit and value.
+
+            Cards = new Queue<Card>();
+
+            for (int i = 2; i <= 14; i++)
+            {
+                Cards.Enqueue(new Card(i, Suit.Clubs));
+                Cards.Enqueue(new Card(i, Suit.Diamonds));
+                Cards.Enqueue(new Card(i, Suit.Hearts));
+                Cards.Enqueue(new Card(i, Suit.Spades));
+            }
+        }
+
+        public void Shuffle()
+        {
+            List<Card> cards = [.. Cards];
+            cards.RandomSort();
+
+            Cards = new Queue<Card>();
+            foreach (Card c in cards)
+            {
+                Cards.Enqueue(c);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (Card card in Cards)
+            {
+                sb.AppendLine(card.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- Core/GenericExtensions.cs
+++ Core/GenericExtensions.cs
@@ -1,10 +1,22 @@
-﻿using System.Data;
-using System.Reflection;
-using System.Security.Cryptography;
-
+﻿
 namespace Core
 {
     public static class GenericExtensions
     {
+        private static readonly Random rnd = new();
+
+        public static void RandomSort<T>(this IList<T> list)
+        {
+            int n = list.Count;
+
+            while (n > 1)
+            {
+                n--;
+
+                int k = rnd.Next(n + 1);
+
+                (list[n], list[k]) = (list[k], list[n]);
+            }
+        }
     }
 }
