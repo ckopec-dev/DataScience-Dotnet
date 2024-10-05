7c0c3582228030133bf3983a9e0e2aa77e139b69
ckopec-dev <c_kopec@hotmail.com>
2024-10-04 22:01:50 -0400

    Added LinearRegression methods and unit tests

---
+++ UnitTests/LinearRegressionUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Core;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class LinearRegressionUnitTests
+    {
+        [TestMethod]
+        public void SlopeTest()
+        {
+            decimal slope = LinearRegression.Slope([5m, 7m, 12m, 16m, 20m], [40m, 120m, 180m, 210m, 240m]);
+
+            Assert.AreEqual(slope, 12.207792207792207792207792208m);
+        }
+
+        [TestMethod]
+        public void InterceptTest()
+        {
+            decimal intercept = LinearRegression.Intercept([5m, 7m, 12m, 16m, 20m], [40m, 120m, 180m, 210m, 240m]);
+
+            Assert.AreEqual(intercept, 11.50649350649350649350649350m);
+        }
+    }
+}
+
