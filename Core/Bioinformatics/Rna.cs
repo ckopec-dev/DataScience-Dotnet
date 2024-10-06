d179c849ed3b32735eabaa4f07c37e5115e46cb0
ckopec-dev <c_kopec@hotmail.com>
2024-10-06 13:04:23 -0400

    Created Rna class

---
+++ Core/Bioinformatics/Rna.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Core.Bioinformatics
+{
+    public class Rna
+    {
+        private string _Code = String.Empty;
+
+        public string Code
+        {
+            get { return _Code; }
+            set
+            {
+                if (IsValidRna(value))
+                    _Code = value;
+                else
+                    throw new ArgumentException("Invalid rna string.");
+            }
+        }
+
+        public Rna()
+        {
+        }
+
+        public Rna(string code)
+        {
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            return Code;
+        }
+
+        public static bool IsValidRna(string code)
+        {
+            string pattern = "^[CAGU]+$";
+            Regex rg = new(pattern);
+
+            return rg.IsMatch(code);
+        }
+    }
+}
---
+++ UnitTests/RnaUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Core.Bioinformatics;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RnaUnitTests
+    {
+        [TestMethod]
+        public void TestValidInitialization()
+        {
+            Rna rna = new("GAUGGAACUUGACUACGUAAAUU");
+            Assert.IsNotNull(rna);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInvalidInitialization()
+        {
+            Rna rna = new("not valid rna");
+        }
+    }
+}
