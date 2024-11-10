f50b0c4a3c63ccc0aca3e35fa603af30b9c81169
ckopec-dev <c_kopec@hotmail.com>
2024-11-10 12:40:51 -0500

    EdgeList update

--- Core/EdgeList.cs
+++ Core/EdgeList.cs
@@ -9,17 +9,43 @@ namespace Core
         // The first line contains two numbers, the number of vertices n and the number of edges m.
         // Each of the following m lines contains an edge given by two vertices. The format can be used to specify directed and undirected graphs as well as weighted and unweighted graphs.
 
+        /* Example:
+        
+        6 7
+        1 2
+        2 3
+        6 3
+        5 6
+        2 5
+        2 4
+        4 1
+
+        */
+
+        // To specify a weighted graph each edge should be followed by its weight.
+
+        /* Example:
+          
+        6 7
+        1 2 4
+        2 3 1
+        6 3 3
+        5 6 2
+        2 5 -2
+        2 4 5
+        4 1 1
+
+        */
+
         public int VertexCount { get; set; }
         public int EdgeCount { get; set; }
 
-        //public
-
-        public EdgeList(List<int[]> lines)
+        public EdgeList(List<string> lines)
         {
-            VertexCount = lines[0][0];
-            EdgeCount = lines[0][1];
+            //VertexCount = lines[0][0];
+            //EdgeCount = lines[0][1];
 
-            //throw new NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public override string? ToString()
