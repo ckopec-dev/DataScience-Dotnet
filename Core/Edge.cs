18ed82e719a70003cc626534e26fb9cc38a915c3
ckopec-dev <c_kopec@hotmail.com>
2024-11-17 11:02:38 -0500

    Edge class updates

--- Core/Edge.cs
+++ Core/Edge.cs
@@ -1,14 +1,15 @@
 ﻿
 namespace Core
 {
-    public class Edge(int vertexA, int vertexB)
+    public class Edge(int vertexA, int vertexB, int? weight = null)
     {
         public int VertexA { get; set; } = vertexA;
         public int VertexB { get; set; } = vertexB;
+        public int? Weight { get; set; } = weight;
 
         public override string ToString()
         {
-            return String.Format("{0} {1}", VertexA, VertexB);
+            return String.Format("{0} {1} {2}", VertexA, VertexB, Weight).Trim();
         }
     }
 }
