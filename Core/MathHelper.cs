8f2867fd6678f36e5da372e2eece8a7ca909f9fc
ckopec-dev <c_kopec@hotmail.com>
2024-12-24 18:43:45 -0500

    Added RSA-129

--- Core/MathHelper.cs
+++ Core/MathHelper.cs
@@ -1,8 +1,14 @@
-﻿
+﻿using System.Numerics;
+
 namespace Core
 {
     public static class MathHelper
     {
+        public static BigInteger RSA129
+        {
+            get { return BigInteger.Parse("114381625757888867669235779976146612010218296721242362562561842935706935245733897830597123563958705058989075147599290026879543541"); }
+        } 
+
         public static bool IsPrime(long n)
         {
             if (n < 2) return false;
