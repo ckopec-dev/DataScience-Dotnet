d8dd7a53e005c34ed47e8bc26d550184c2652ef2
ckopec-dev <c_kopec@hotmail.com>
2025-02-12 21:46:44 -0500

    Trader DAL built

--- Soho/Program.cs
+++ Soho/Program.cs
@@ -5,15 +5,6 @@
         static void Main()
         {
             // See https://sungrazer.nrl.navy.mil/tutorial
-
-            // General algo idea rev 1...
-
-            // Download sequential images.
-            // Load as bitmaps so individual pixels can be calculated. (how many? 3-5?)
-            // Divide image into square blocks. (how small?). ignore certain regions that have static values which will create false positives (e.g. the center and corners? not sure if this is necessary)
-            // Compare "whiteness" of neighboring blocks. Is it significantly higher? (how significant?)
-            // Does a similar block of whiteness appear in the subsequent image, but in a different location? (the match has to be somewhat loose to account for noise)
-            // Render images with detection results to help visualize/analyze algo output?
         }
     }
 }
