4679287067a612a95cef4857e694bf73f9cc5809
ckopec-dev <c_kopec@hotmail.com>
2025-02-20 13:28:12 -0500

    Added Core.DataHelper

---
+++ Core/DataHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Core
+{
+    class DataHelper
+    {
+        public static List<DateOnly> USStockMarketHolidays
+        {
+            get
+            {
+                Stream? mrs = Assembly.GetExecutingAssembly().GetManifestResourceStream("Core.Data.USStockMarketHolidays.txt") ?? throw new Exception("Resource not found: hamm.txt");
+                using StreamReader sr = new(mrs);
+
+                List<DateOnly> dates = [];
+                string? line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    dates.Add(DateOnly.Parse(line.Trim()));
+                }
+
+                return dates;
+            }
+        }
+}
+}
