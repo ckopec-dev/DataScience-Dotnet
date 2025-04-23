390b2c04ff9c7dafccc26ed35bcadbe714afed97
ckopec-dev <c_kopec@hotmail.com>
2025-04-23 15:24:43 -0400

    Rosalind BFS, progress

--- Core/DirectedGraph.cs
+++ Core/DirectedGraph.cs
@@ -1,15 +1,57 @@
 ﻿
+using System.Diagnostics.CodeAnalysis;
+
 namespace Core
 {
     public class DirectedGraph
     {
+        public int Vertices { get; set; }
+        public int Edges { get; set; }
+        public List<int> Origins = [];
+        public List<int> Destinations = [];
+
         public DirectedGraph()
         {
         }
 
-        public DirectedGraph(string edgeListFormat)
+        public DirectedGraph(StreamReader sr)
         {
-            throw new NotImplementedException();
+            string? line = sr.ReadLine() ?? throw new InvalidInputException();
+            string[] header = line.Split(' ');
+            Vertices = int.Parse(header[0]);
+            Edges = int.Parse(header[1]);
+
+            while(!sr.EndOfStream)
+            {
+                line = sr.ReadLine() ?? throw new InvalidInputException();
+
+                string[] split = line.Split(' ');
+                Origins.Add(Convert.ToInt32(split[0])); 
+                Destinations.Add(Convert.ToInt32(split[1]));
+            }
+        }
+
+        public List<int> PathsFrom(int destinationVertexNumber, int depth)
+        {
+            // e.g. vertextNumber 5. 
+            // Find all destinations with that value.
+
+            List<int> originIndexes = [];
+            int newDepth = depth + 1;
+
+            for (int i = 0; i < Destinations.Count; i++)
+            {
+                if (Destinations[i] == destinationVertexNumber)
+                {
+                    // this is a path. find next path.
+                    Console.WriteLine("Destination: {0}, Origin: {1}, Depth: {2}", destinationVertexNumber, Origins[i], depth);
+                    originIndexes.Add(i);
+
+                    originIndexes.AddRange(PathsFrom(Origins[i], newDepth));
+                }
+            }
+
+            return originIndexes;
         }
     }
 }
