5e001938d92d7fd4f138285a5fdf189800b72aea
ckopec-dev <c_kopec@hotmail.com>
2025-07-16 17:13:28 -0400

    Internet changes

--- Core/Internet/WhoIsClient.cs
+++ Core/Internet/WhoIsClient.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Internet
 {
-    internal class WhoIsClient
+    public class WhoIsClient
     {
+        public static void Query()
+        {
+            string domain = "yahoo.com";
+
+            try
+            {
+                string whoisServer = "whois.verisign-grs.com"; // For .com/.net domains
+                int port = 43;
+
+                using (TcpClient client = new TcpClient(whoisServer, port))
+                using (NetworkStream stream = client.GetStream())
+                using (StreamWriter writer = new StreamWriter(stream))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    writer.WriteLine(domain);
+                    writer.Flush();
+
+                    string response = reader.ReadToEnd();
+                    Console.WriteLine("WHOIS Response:\n");
+                    Console.WriteLine(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
     }
 }
