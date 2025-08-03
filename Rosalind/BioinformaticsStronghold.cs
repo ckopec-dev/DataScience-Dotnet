5e378a7b4e4b8cc078c1bec2601408ecb6ba3704
ckopec-dev <c_kopec@hotmail.com>
2025-08-03 17:24:58 -0400

    Rosalind problem FIBD, solved

--- Rosalind/BioinformaticsStronghold.cs
+++ Rosalind/BioinformaticsStronghold.cs
@@ -275,8 +275,8 @@ namespace Rosalind
             // Given: Positive integers n ≤ 100 and m ≤ 20.
             // Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months. 1 pair per litter.
 
-            int totalMonths = 6;
-            int lifespan = 3;
+            int totalMonths = 92;
+            int lifespan = 20;
 
             BigInteger[] newRabbits = new BigInteger[totalMonths];
             BigInteger[] matureRabbits = new BigInteger[totalMonths];
