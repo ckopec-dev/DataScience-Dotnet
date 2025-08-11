967200dff337886adf698e7fafc0ba27497779f3
ckopec-dev <c_kopec@hotmail.com>
2025-08-10 20:07:51 -0400

    Build script updates

--- Euler/postbuild.bat
+++ Euler/postbuild.bat
@@ -7,9 +7,6 @@ echo Configuration: %4
 
 if "%1"=="Debug" ( 
 	echo Copying output to debug directory.
-) else if "%1" == "Release" (
-	echo Copying output to //rm001/sata08/intranet/euler directory.
-	xcopy "%2%3\bin\%4\net8.0" "p:\euler\" /s /i /y 
 ) else (
 	echo Configuration not implemented in postbuild.
 )
